c1b308b881f65e250b41f9381a9a3c9f4cf1dfb5
Rodrigo Peixoto <rodrigopex@gmail.com>
2025-11-25 20:05:08 -0300

    Add hello zbus sample
    Signed-off-by: Rodrigo Peixoto <rodrigopex@citrinio.com>

---
+++ Day3/solutions/hello_zbus/src/hello_msg_subscriber.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Citrinio
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdint.h>
+#include <zephyr/kernel.h>
+#include <zephyr/zbus/zbus.h>
+
+ZBUS_MSG_SUBSCRIBER_DEFINE(msub_01);
+
+ZBUS_CHAN_DEFINE(chan_01, uint16_t, NULL, NULL, ZBUS_OBSERVERS(msub_01), 0);
+
+int main(void)
+{
+	uint16_t data = 10;
+
+	zbus_chan_pub(&chan_01, &data, K_NO_WAIT);
+
+	return 0;
+}
+
+void zbus_subscriber_thread_main(void *p1, void *p2, void *p3)
+{
+	ARG_UNUSED(p1);
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	uint16_t data;
+	const struct zbus_channel *chan;
+
+	while (1) {
+		zbus_sub_wait_msg(&msub_01, &chan, &data, K_FOREVER);
+
+		printk("Hello MSG Subscriber! Received %d from %s\n", data, zbus_chan_name(chan));
+	}
+}
+
+K_THREAD_DEFINE(sub_thread, 1024, zbus_subscriber_thread_main, NULL, NULL, NULL, 4, 0, 0);
